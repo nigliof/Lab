41ad18489dc032487c043b3294885c1bac65f3ff
Felipe Niglio <felipe.niglio@gmail.com>
2024-11-20 19:10:58 -0300

    Editado models e testar não subir settings.py

--- labman/escala/models.py
+++ labman/escala/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+sexo_choices = (("M", "MASCULINO"),("F", "FEMININO"))
 
 class funcionario(models.Model):
     nome_completo = models.CharField(max_length=200)
+    sexo = models.CharField(max_length=15, choices=sexo_choices)
